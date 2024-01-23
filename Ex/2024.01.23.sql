c00fa3d2ad15400a93232291620d48180e1ba141
dasom <dasom589@gmail.com>
2024-01-23 13:11:28 +0900

    2024-01-23 4교시

--- Ex/2024.01.23.sql
+++ Ex/2024.01.23.sql
@@ -83,3 +83,44 @@ group by department_id
 having sum(salary)>=20000	-- having절에는 그룹함수, group by에 참려한 컬럼만 사용 가능
 -- and department_id=100
 ;
+
+-- 조건문
+select  first_name
+		, commission_pct
+        , if(commission_pct is null, 0, 1) state
+from employees
+;
+
+/*
+직원아이디, 월급, 업무아이디, 실제월급(realSalary)을 출력하세요.
+실제월급은 job_id가 'AC_ACCOUNT' 면 월급+월급*0.1,
+				'SA_REP' 월급+월급*0.2,
+				'ST_CLERK' 면 월급+월급*0.3
+				그외에는 월급으로 계산하세요
+*/
+select  employee_id,
+		salary,
+        job_id,
+        case  when job_id='AC_ACCOUNT' then salary+salary*0.1
+			  when job_id='SA_REP' then salary+salary*0.2
+              when job_id='ST_CLERK' then salary+salary*0.3
+              else salary
+		end realSalary
+from employees
+;
+
+/*
+직원의 이름, 부서번호, 팀을 출력하세요
+팀은 코드로 결정하며 부서코드가
+10~50 이면 'A-TEAM'
+60~100이면 'B-TEAM'
+110~150이면 'C-TEAM'
+나머지는 '팀없음'
+으로 출력하세요.
+*/
+select  first_name,
+		department_id,
+		case when 10<=department_id<=50 then 'A-TEAM'
+			 when 
+from employees
+;
